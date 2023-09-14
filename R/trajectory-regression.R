405b01a69057b157755c45223c4e9b3d48e92a5c
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2023-09-14 16:13:52 +0300

    fix: use na.if.notfound for gintervals.neighbors

--- R/trajectory-regression.R
+++ R/trajectory-regression.R
@@ -213,7 +213,7 @@ regress_trajectory_motifs <- function(atac_scores,
             cli_abort("Please make sure the current genome ({.field {GROOT}}) has an intervals set called {.val intervs.global.tss}")
         }
 
-        tss_dist <- abs(misha::gintervals.neighbors(peak_intervals, "intervs.global.tss")$dist)
+        tss_dist <- abs(misha::gintervals.neighbors(peak_intervals, "intervs.global.tss", na.if.notfound = TRUE)$dist)
         enhancers_filter <- tss_dist > min_tss_distance
         if (sum(!enhancers_filter) > 0) {
             cli_alert_info("{.val {sum(!enhancers_filter)}} peaks were filtered out because they are too close to TSS (<= {.val min_tss_distance}bp)")
