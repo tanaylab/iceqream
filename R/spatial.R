a9ca9b7a0ff33b2b8832a7a83b3a9caf0f2bc963
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2023-09-19 12:23:15 +0300

    interim commit

--- R/spatial.R
+++ R/spatial.R
@@ -1,3 +1,145 @@
+compute_spat_pwm <- function(pssm, intervals = NULL, size = NULL, sequences = NULL, bidirect = TRUE, min_energy = -10, q = 1) {
+    if (is.null(sequences)) {
+        if (is.null(intervals)) {
+            cli_abort("Either {.field {intervals}} or {.field {sequences}} must be provided.")
+        }
+
+        if (!is.null(size)) {
+            intervals <- misha.ext::gintervals.normalize(intervals %>% select(any_of(c("chrom", "start", "end", "strand"))), size)
+        }
+
+        withr::local_options(list(gmax.data.size = 1e9))
+
+        sequences <- toupper(misha::gseq.extract(intervals))
+    }
+
+    local_pwm <- prego::compute_local_pwm(sequences, pssm, bidirect = bidirect)
+
+    local_pwm_n <- norm_energy(local_pwm, min_energy = min_energy, q = q)
+
+    return(local_pwm_n)
+}
+
+direct_sequences <- function(sequences, pssm) {
+    sequences_rc <- chartr("acgtACGT", "tgcaTGCA", sequences) %>%
+        stringi::stri_reverse()
+
+    size <- nchar(sequences[1])
+    middle_point <- round(size / 2)
+
+    s_l <- substr(sequences, 1, middle_point)
+    s_r <- substr(sequences, (middle_point + 1), size)
+
+    l_pwm <- prego::compute_pwm(s_l, pssm, bidirect = TRUE)
+    r_pwm <- prego::compute_pwm(s_r, pssm, bidirect = TRUE)
+
+    # for each sequence - if r > l reverse complement
+    new_sequences <- ifelse(r_pwm > l_pwm, sequences_rc, sequences)
+
+    return(new_sequences)
+}
+
+#' Compute motif directional hits
+#'
+#' This function computes the number of hits of a given motif in a set of intervals, separated by strand and direction.
+#'
+#' @param pssm A position-specific scoring matrix (PSSM) object.
+#' @param intervals A data frame containing the genomic intervals to search for motif hits.
+#' @param size The size of the intervals.
+#' @param pwm_threshold The threshold for the PSSM score to consider a hit.
+#'
+#' @return A data frame containing the number of hits of the motif in each interval, separated by strand and direction. Column names are 'ml' (minus, left), 'mr' (minus, right), 'pl' (plus, left), 'pr' (plus, right).
+#'
+#' @export
+compute_motif_directional_hits <- function(pssm, intervals, size, pwm_threshold = 7) {
+    pwm_minus <- compute_spat_pwm(pssm, intervals %>% mutate(strand = -1), size, bidirect = FALSE)
+    pwm_plus <- compute_spat_pwm(pssm, intervals %>% mutate(strand = 1), size, bidirect = FALSE)
+    freqs_plus <- pwm_plus >= pwm_threshold
+    freqs_minus <- pwm_minus >= pwm_threshold
+
+    middle_point <- round(size / 2)
+
+    # count # of hits at the left and at the right, for plus and minus.
+    hits <- matrix(0, nrow = 4, ncol = nrow(intervals))
+    hits[1, ] <- rowSums(freqs_minus[, (middle_point + 1):size], na.rm = TRUE)
+    hits[2, ] <- rowSums(freqs_minus[, 1:middle_point], na.rm = TRUE)
+    hits[3, ] <- rowSums(freqs_plus[, 1:middle_point], na.rm = TRUE)
+    hits[4, ] <- rowSums(freqs_plus[, (middle_point + 1):size], na.rm = TRUE)
+
+    hits <- t(hits)
+    colnames(hits) <- c("ml", "mr", "pl", "pr")
+
+    return(as.data.frame(hits))
+}
+
+annotate_intervals_diff_score <- function(intervals, diff_score, top_q = 0.1, bottom_q = 0.1) {
+    intervals <- intervals %>%
+        mutate(diff_score = !!diff_score) %>%
+        arrange(desc(diff_score)) %>%
+        mutate(type = ifelse(row_number() <= nrow(intervals) * top_q, "top", ifelse(row_number() > nrow(intervals) * (1 - bottom_q), "bottom", "middle"))) %>%
+        filter(type != "middle") %>%
+        select(-diff_score)
+    return(intervals)
+}
+
+compute_motif_pwm_threshold <- function(intervals, pssm, q_thresh = 0.95, diff_score = NULL, top_q = 0.1, bottom_q = 0.1, size = 1000) {
+    if (!("type" %in% colnames(intervals))) {
+        intervals <- annotate_intervals_diff_score(intervals, diff_score = diff_score, top_q = top_q, bottom_q = bottom_q)
+    }
+    pwms <- compute_spat_pwm(pssm, intervals, size, bidirect = TRUE)
+
+    top_pwms <- pwms[intervals$type == "top", ]
+    bottom_pwms <- pwms[intervals$type == "bottom", ]
+
+    if (mean(top_pwms, na.rm = TRUE) > mean(bottom_pwms, na.rm = TRUE)) {
+        thresh <- quantile(bottom_pwms, q_thresh, na.rm = TRUE)
+    } else {
+        thresh <- quantile(top_pwms, q_thresh, na.rm = TRUE)
+    }
+
+    return(thresh)
+}
+
+compute_traj_model_directional_hits <- function(traj_model, size, pwm_quantile = 0.999, top_q = 0.1, bottom_q = 0.1, parallel = TRUE) {
+    intervals <- traj_model@peak_intervals
+
+    intervals <- annotate_intervals_diff_score(intervals, traj_model@diff_score, top_q, bottom_q)
+
+    hits <- plyr::llply(names(traj_model@motif_models), function(motif) {
+        pssm <- traj_model@motif_models[[motif]]$pssm
+        cli::cli_alert("Computing directional hits for {.val {motif}}")
+        pwm_threshold <- compute_motif_pwm_threshold(intervals, pssm, diff_score = traj_model@diff_score, q_thresh = pwm_quantile, size = size)
+        cli::cli_alert_info("PWM threshold for {.val {motif}}: {.val {pwm_threshold}}")
+        top <- compute_motif_directional_hits(
+            pssm = pssm,
+            intervals = intervals %>% filter(type == "top"),
+            size = size,
+            pwm_threshold = pwm_threshold
+        )
+        bottom <- compute_motif_directional_hits(
+            pssm = pssm,
+            intervals = intervals %>% filter(type == "bottom"),
+            size = size,
+            pwm_threshold = pwm_threshold
+        )
+
+        colnames(top) <- paste0(colnames(top), ".", motif)
+        colnames(bottom) <- paste0(colnames(bottom), ".", motif)
+        list(
+            top = top,
+            bottom = bottom,
+            threshold = pwm_threshold
+        )
+    }, .parallel = parallel)
+
+    top_mat <- do.call(cbind, lapply(hits, function(x) x$top))
+    bottom_mat <- do.call(cbind, lapply(hits, function(x) x$bottom))
+    thresholds <- sapply(hits, function(x) x$threshold)
+
+    return(list(top = as.matrix(top_mat), bottom = as.matrix(bottom_mat), threshold = thresholds))
+}
+
+
 compute_pssm_spatial_freq <- function(pssm, intervals = NULL, size = NULL, pwm_threshold = 7, sequences = NULL, atac_track = NULL) {
     if (is.null(sequences)) {
         if (is.null(intervals)) {
@@ -25,6 +167,9 @@ compute_pssm_spatial_freq <- function(pssm, intervals = NULL, size = NULL, pwm_t
     n_hits <- sum(freqs, na.rm = TRUE)
 
     if (!is.null(atac_track)) {
+        if (is.null(intervals)) {
+            cli_abort("Intervals must be provided when computing ATAC-seq frequency.")
+        }
         atac <- gextract(atac_track, iterator = 1, intervals = intervals, colnames = "v")
         atac_mat <- atac %>%
             arrange(intervalID) %>%
@@ -121,7 +266,7 @@ plot_motif_spatial_freq <- function(spatial_freqs, motif, smooth = 10, plot_atac
 
     p <- ggplot(spatial_freqs, aes(x = pos, y = freq_roll, color = type)) +
         geom_line() +
-        scale_color_manual(name = "", values = c(bottom = "red", top = "blue")) +
+        scale_color_manual(name = "", values = c(top = "red", bottom = "blue")) +
         theme_classic() +
         theme(legend.position = c(0.9, 0.9)) +
         xlab("Position") +
@@ -149,8 +294,8 @@ plot_motif_spatial_freq <- function(spatial_freqs, motif, smooth = 10, plot_atac
             pull(n_hits)
 
         # annotate with the N_hits + %
-        p <- p + annotate("text", x = 1, y = 0.9 * max(spatial_freqs$freq_roll, na.rm = TRUE), label = glue::glue("N = {n_hits_top} ({round(n_hits_top / n_top, 2)})"), hjust = 0, size = 3, color = "blue")
-        p <- p + annotate("text", x = 1, y = 0.75 * max(spatial_freqs$freq_roll, na.rm = TRUE), label = glue::glue("N = {n_bottom_hits} ({round(n_bottom_hits / n_bottom, 2)})"), hjust = 0, size = 3, color = "red")
+        p <- p + annotate("text", x = 1, y = 0.9 * max(spatial_freqs$freq_roll, na.rm = TRUE), label = glue::glue("N = {n_hits_top} ({round(n_hits_top / n_top, 2)})"), hjust = 0, size = 3, color = "red")
+        p <- p + annotate("text", x = 1, y = 0.75 * max(spatial_freqs$freq_roll, na.rm = TRUE), label = glue::glue("N = {n_bottom_hits} ({round(n_bottom_hits / n_bottom, 2)})"), hjust = 0, size = 3, color = "blue")
     }
 
     return(p)
@@ -180,4 +325,24 @@ plot_all_motif_spatial_freq <- function(traj_model, size, pwm_threshold = 7, top
     } else {
         return(p)
     }
+}
+
+compute_fold_enrichment <- function(mat) {
+    mat[is.na(mat)] <- 0
+
+    mat <- mat > 0
+
+    # Get the probabilities of having a 1 in each column
+    p_col <- colSums(mat) / nrow(mat)
+
+    # Get the probability of having a 1 in both columns using matrix multiplication
+    p_both <- (t(mat) %*% mat) / nrow(mat)
+
+    # Get the outer product of the column probabilities to get the denominator for the fold enrichment calculation
+    p_first_second <- tcrossprod(p_col)
+
+    # Calculate fold enrichment
+    fe_matrix <- log2(p_both / p_first_second)
+
+    return(fe_matrix)
 }
