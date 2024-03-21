b24abd1a558a5535976a3c19493d256e4a5449c0
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2024-03-21 23:17:36 +0200

    fix: as.matrix in prego inference normalization

--- R/prego.R
+++ R/prego.R
@@ -73,7 +73,7 @@ learn_traj_prego <- function(peak_intervals, atac_diff, n_motifs, min_diff = 0.2
 
     prego_e <- reg$predict_multi(sequences)
     prego_norm_e <- reg$predict_multi(norm_seqs)
-    prego_e <- norm_energy_matrix(prego_e, prego_norm_e, min_energy = min_energy, q = energy_norm_quantile, norm_energy_max = norm_energy_max)
+    prego_e <- norm_energy_matrix(as.matrix(prego_e), as.matrix(prego_norm_e), min_energy = min_energy, q = energy_norm_quantile, norm_energy_max = norm_energy_max)
 
     prego_models <- prego::export_multi_regression(reg)$models
     names(prego_models) <- colnames(prego_e)
