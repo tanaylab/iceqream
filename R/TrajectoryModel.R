ca98d9311037c68a114c94e90d6b7ff8ff990030
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2024-03-28 11:37:11 +0200

    fixed TrajectoryModel print function

--- R/TrajectoryModel.R
+++ R/TrajectoryModel.R
@@ -87,7 +87,7 @@ setMethod("show", signature = "TrajectoryModel", definition = function(object) {
         cli_ul(c("{.field @predicted_diff_score}: A numeric value representing the predicted difference score"))
         cli_ul(c("{.field @initial_prego_models}: A list of prego models used in the initial phase of the algorithm ({.val {length(object@initial_prego_models)}} models)"))
         cli_ul(c("{.field @peak_intervals}: A data frame containing the peak intervals ({.val {nrow(object@peak_intervals)}} elements)"))
-        if ("norm_intervals" %in% slotNames(object)) { # here for backwards compatibility
+        if ("normalization_intervals" %in% slotNames(object)) { # here for backwards compatibility
             cli_ul(c("{.field @normalization_intervals}: A data frame containing the intervals used for energy normalization ({.val {nrow(object@normalization_intervals)}} elements)"))
         }
         if (length(object@features_r2) > 0) {
