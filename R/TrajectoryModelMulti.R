75d1404e93958c236ec65ac28cca6654b9b9c4f7
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2024-04-09 07:54:53 +0300

    added models_full to multi trajectory model

--- R/TrajectoryModelMulti.R
+++ R/TrajectoryModelMulti.R
@@ -3,6 +3,7 @@
 #' This class represents a multi-trajectory model, which is a collection of individual trajectory models.
 #'
 #' @slot models A list of individual trajectory models.
+#' @slot models A list of individual trajectory models with full features.
 #' @slot motif_models A list of motif models corresponding to the individual trajectory models.
 #' @slot cluster_map a data frame that maps original motif to its distilled cluster.
 #' @slot stats A data frame with r2 statistics for each trajectory model after distillation.
@@ -13,6 +14,7 @@ TrajectoryModelMulti <- setClass(
     "TrajectoryModelMulti",
     slots = list(
         models = "list",
+        models_full = "list",
         motif_models = "list",
         cluster_map = "data.frame",
         stats = "data.frame",
@@ -30,6 +32,7 @@ setMethod(
             cli::cli_text("\n")
             cli::cli_text("Slots include:")
             cli_ul(c("{.field @models}: A list of individual trajectory models ({.val {length(object@models)}} models). models: {.val {names(object@models)}}."))
+            cli_ul(c("{.field @models_full}: A list of individual trajectory models with full features ({.val {length(object@models_full)}} models: {.val {names(object@models_full)}})."))
             cli_ul(c("{.field @motif_models}: A named list of motif models. Each element contains PSSM and spatial model ({.val {length(object@motif_models)}} models: {.val {names(object@motif_models)}})."))
             cli_ul(c("{.field @cluster_map}: A data frame that maps original motif to its distilled cluster."))
             cli_ul(c("{.field @stats}: A data frame with r2 statistics for each trajectory model after distillation."))
--- R/report-multi-traj.R
+++ R/report-multi-traj.R
@@ -1,6 +1,47 @@
-plot_multi_traj_model_report <- function(multi_traj, filename = NULL, width = NULL, height = NULL, dev = grDevices::pdf, title = NULL, ...) {
+#' Plot multi-trajectory model report
+#'
+#' This function plots a report for a multi-trajectory model.
+#'
+#' @param multi_traj The multi-trajectory model object.
+#' @param filename Optional. The name of the output file. If not provided, the plot will be displayed on the screen.
+#' @param width Optional. The width of the output plot in inches.
+#' @param height Optional. The height of the output plot in inches.
+#' @param dev Optional. The graphics device to use for output. Default is 'pdf'.
+#' @param title Optional. The title of the plot.
+#' @param use_full Optional. Whether to use the models with all the motifs or not. Default is FALSE.
+#' @param ... Additional arguments to be passed to the plotting function.
+#'
+#' @return A ggplot2 object (invsibly if a filename is provided).
+#'
+#'
+#' @export
+plot_multi_traj_model_report <- function(multi_traj, filename = NULL, width = NULL, height = NULL, dev = grDevices::pdf, title = NULL, use_full = FALSE, ...) {
     models <- multi_traj@motif_models
+    
+
     motif_num <- length(models)
+
+    if (use_full) {
+        partial_resp_list <- purrr::map(multi_traj@models_full, compute_partial_response)
+        traj_models <- multi_traj@models_full
+    } else {
+        partial_resp_list <- purrr::map(multi_traj@models, compute_partial_response)
+        traj_models <- multi_traj@models
+    }
+
+    pr_mat <- partial_resp_list %>%
+        purrr::imap_dfr(~ enframe(matrixStats::colMaxs(as.matrix(.x)), "var", "max_pr") %>% mutate(model = .y)) %>%
+        select(var, max_pr, model) %>%
+        filter(var %in% names(models)) %>%
+        pivot_wider(names_from = c(model), values_from = max_pr) %>%
+        tibble::column_to_rownames("var") %>%
+        as.matrix()
+    pr_mat[is.na(pr_mat)] <- 0
+    cm <- tgs_cor(t(pr_mat), pairwise.complete.obs = TRUE)
+    hc <- hclust(as.dist(1 - cm), method = "ward.D2")
+    models <- models[hc$order]
+
+
     spatial_p <- purrr::imap(models, ~ {
         if (is.null(.x$spat)) {
             return(plot_fake_spat())
@@ -10,21 +51,19 @@ plot_multi_traj_model_report <- function(multi_traj, filename = NULL, width = NU
 
     motifs_p <- purrr::imap(models, ~ prego::plot_pssm_logo(.x$pssm, title = .y))
 
-    partial_resp_list <- purrr::map(multi_traj@models, compute_partial_response)
-
     pr_p <- purrr::map(names(models), function(motif) {
-        purrr::map(names(multi_traj@models), function(model) {
+        purrr::map(names(traj_models), function(model) {
             pr <- partial_resp_list[[model]]
             if (!(motif %in% names(pr))) {
                 return(plot_fake_pr() + ggtitle(model))
             }
-            plot_e_vs_pr(motif, pr, multi_traj@models[[model]]) + ggtitle(model)
+            plot_e_vs_pr(motif, pr, traj_models[[model]]) + ggtitle(model)
         }) %>%
             purrr::discard(is.null) %>%
             patchwork::wrap_plots(nrow = 1)
     })
 
-    n_traj_models <- length(multi_traj@models)
+    n_traj_models <- length(traj_models)
 
     p <- patchwork::wrap_plots(
         A = patchwork::wrap_plots(motifs_p, ncol = 1),
