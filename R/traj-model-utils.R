a686dc1c859ec1dd81fe1447fe1f9b47afb6e027
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2024-05-16 15:51:27 +0300

    bug fixes in relearn_traj_model

--- R/traj-model-utils.R
+++ R/traj-model-utils.R
@@ -1,35 +1,52 @@
 #' Relearn Trajectory Model
 #'
-#' This function relearns a trajectory model using the glmnet package.
+#' This function relearns a trajectory model using the glmnet package, without re-inferring the motif models.
 #'
 #' @param traj_model The trajectory model object.
 #' @param new_energies If TRUE - recreate the energies. Default is FALSE.
 #' @param new_logist If TRUE - recreate the logistic features. Default is FALSE. If \code{new_energies} is TRUE, this is automatically set to TRUE.
 #' @param lambda The lambda value to use for relearning. If NULL, the lambda value from the trajectory model is used.
+#' @param use_additional_features A logical value indicating whether to use additional features. Default is TRUE.
+#' @param use_motifs A logical value indicating whether to use motif models. Default is TRUE.
 #' @param verbose Logical indicating whether to display additional information.
 #' @return The updated trajectory model object.
 #'
 #' @export
-relearn_traj_model <- function(traj_model, new_energies = FALSE, new_logist = FALSE, lambda = NULL, verbose = FALSE) {
+relearn_traj_model <- function(traj_model, new_energies = FALSE, new_logist = FALSE, lambda = NULL, use_additional_features = TRUE, use_motifs = TRUE, verbose = FALSE) {
     if (new_energies) {
-        traj_model@normalized_energies <- calc_traj_model_energies(traj_model)
+        traj_model@normalized_energies[traj_model@type == "train", ] <- calc_traj_model_energies(traj_model, traj_model@peak_intervals[traj_model@type == "train", ])
+        traj_model@normalized_energies[traj_model@type == "test", ] <- calc_traj_model_energies(traj_model, traj_model@peak_intervals[traj_model@type == "test", ])
         new_logist <- TRUE
     }
 
     if (new_logist) {
-        X <- cbind(
-            create_logist_features(traj_model@normalized_energies),
-            traj_model@additional_features
-        )
-    } else {
-        X <- traj_model@model_features
+        if (use_additional_features) {
+            add_feats <- traj_model@additional_features[rownames(traj_model@normalized_energies), ]
+            if (use_motifs) {
+                traj_model@model_features <- as.matrix(cbind(
+                    create_logist_features(traj_model@normalized_energies),
+                    create_logist_features(traj_model@additional_features)
+                ))
+            } else {
+                traj_model@model_features <- create_logist_features(traj_model@additional_features)
+                traj_model@motif_models <- list()
+                traj_model@normalized_energies <- matrix()
+            }
+        } else {
+            if (!use_motifs) {
+                cli_abort("No features to use for relearning.")
+            }
+            traj_model@additional_features <- data.frame()
+            traj_model@model_features <- create_logist_features(traj_model@normalized_energies)
+        }
     }
+
+    X <- traj_model@model_features
 
     if (is.null(lambda)) {
         lambda <- traj_model@params$lambda
     }
 
-    X <- traj_model@model_features
     y <- norm01(traj_model@diff_score)
 
     X_train <- X[traj_model@type == "train", ]
@@ -94,6 +111,28 @@ remove_motif_models_from_traj <- function(traj_model, motif_models, verbose = TR
     return(traj_model)
 }
 
+remove_additional_feature_from_traj <- function(traj_model, feature_name, verbose = TRUE) {
+    X <- traj_model@model_features
+    vars <- colnames(traj_model@additional_features)
+
+    if (!feature_name %in% vars) {
+        cli_abort("Feature {.val {feature_name}} not found in the trajectory model.")
+    }
+
+    X_f <- X[, grep(paste0("(", feature_name, ")"), colnames(X), invert = TRUE)]
+
+    traj_model@model_features <- X_f
+    traj_model@additional_features <- traj_model@additional_features[, vars[vars != feature_name], drop = FALSE]
+
+    traj_model <- relearn_traj_model(traj_model, verbose = FALSE)
+    r2_f <- cor(traj_model@predicted_diff_score, norm01(traj_model@diff_score))^2
+    if (verbose) {
+        cli_alert_info("R^2 after removing {.field {feature_name}}: {.val {r2_f}}")
+    }
+
+    return(traj_model)
+}
+
 sample_model <- function(traj_model, sample_frac = 0.1, seed = 60427, verbose = FALSE) {
     traj_model_s <- traj_model
     idxs <- prego::sample_quantile_matched_rows(as.data.frame(traj_model_s@model_features) %>% mutate(id = 1:n()), traj_model_s@diff_score, sample_frac = sample_frac, num_quantiles = 10, seed = seed, verbose = FALSE) %>% pull(id)
@@ -113,11 +152,13 @@ sample_model <- function(traj_model, sample_frac = 0.1, seed = 60427, verbose = 
 #'
 #' @param traj_model The trajectory model object.
 #' @param sample_frac The fraction of samples to use for computing the r2 without each model. When NULL, all samples are used.
+#' @param additional calculate also for additional features. Default is FALSE.
+#' @param seed The seed to use for sampling the data. Default is 60427.
 #'
 #' @return The trajectory model object with the added R-squared values.
 #'
 #' @export
-add_features_r2 <- function(traj_model, sample_frac = 0.1, seed = 60427) {
+add_features_r2 <- function(traj_model, sample_frac = 0.1, additional = FALSE, seed = 60427) {
     traj_model_full <- traj_model
     if (!is.null(sample_frac)) {
         traj_model <- sample_model(traj_model, sample_frac = sample_frac, seed = seed, verbose = TRUE)
@@ -136,10 +177,22 @@ add_features_r2 <- function(traj_model, sample_frac = 0.1, seed = 60427) {
     vars_r2 <- purrr::map_dbl(var_stats, ~ .x$r2)
     names(vars_r2) <- motif_models
 
+    if (additional) {
+        add_r2 <- purrr::map_dbl(colnames(traj_model@additional_features), function(var) {
+            traj_model_f_var <- remove_additional_feature_from_traj(traj_model, var, verbose = FALSE)
+            r2 <- cor(traj_model_f_var@predicted_diff_score, traj_model_f_var@diff_score)^2
+            cli::cli_alert("R^2 added by {.field {var}}: {.val {full_model_r2 - r2}}")
+            r2
+        })
+        names(add_r2) <- colnames(traj_model@additional_features)
+        vars_r2 <- c(vars_r2, add_r2)
+    }
+
     traj_model_full@features_r2 <- full_model_r2 - vars_r2
 
     return(traj_model_full)
 }
+
 
 calc_features_bits <- function(traj_model) {
     bits <- purrr::map_dbl(traj_model@motif_models, ~ sum(prego::bits_per_pos(.x$pssm), na.rm = TRUE))
